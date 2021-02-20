4e54c765c1e97ce9a3ef4dfb39a10bb42430c60a
Amanda Gibson <amanda.gibson@metry.io>
2021-02-20 18:14:08 +0100

    adds front end support for showing votes, adds room reset, modifies styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,57 @@
-"use strict";
+'use strict';
 
 const socket = io();
 let roomId;
 
-const selections = document.getElementById("selections");
-selections.style.display = "none";
+const resetVotesButton = document.getElementById('reset-button');
+resetVotesButton.style.display = 'none';
 
-document.getElementById("start").addEventListener("click", () => {
-    roomId = prompt("Please give your room a name before starting the game:");
-    while (roomId === "") {
-        roomId = prompt("Your room name cannot be left blank. Please enter a room name:");
+const voteResultsContainer = document.getElementById('vote-results-container');
+voteResultsContainer.style.display = 'none';
+
+document.getElementById('start').addEventListener('click', () => {
+    roomId = prompt('Please give your room a name before starting the game:');
+    while (roomId === '') {
+        roomId = prompt(
+            'Your room name cannot be left blank. Please enter a room name:'
+        );
     }
-    socket.emit("start", { roomId: roomId });
-    changeRoomSetup();
+    if (roomId != null) {
+        socket.emit('start', { roomId: roomId });
+        showVotingUI();
+    }
 });
 
-function changeRoomSetup() {
-    start.style.display = "none";
-    document.getElementById("room-name").textContent = `(Room: ${roomId})`;
-    document.getElementById("room-greeting").textContent = `Time to vote!`;
-    selections.style.display = "block";
-};
+function showVotingUI() {
+    start.style.display = 'none';
+    document.getElementById('room-name').textContent = `(Room: ${roomId})`;
+    document.getElementById('room-greeting').textContent = `Time to vote!`;
+    voteResultsContainer.style.display = 'block';
+    resetVotesButton.style.display = 'block';
+}
 
-document.querySelectorAll(".vote-btn").forEach(item => {
-    item.addEventListener("click", () => {
-        socket.emit("vote", { value: `${item.textContent}`, roomId: roomId });
+document.querySelectorAll('.vote-btn').forEach(item => {
+    item.addEventListener('click', () => {
+        socket.emit('vote', { value: `${item.textContent}`, roomId: roomId });
     });
 });
 
-socket.on('currentVotes', (currentVotes) => {
-    console.log(currentVotes)
-})
+resetVotesButton.addEventListener('click', () => {
+    socket.emit('resetVotes', { roomId: roomId });
+});
+
+socket.on('currentVotes', currentVotes => {
+    document.querySelectorAll('.vote-result').forEach(item => {
+        item.innerHTML = null;
+    });
+    const voteCounts = new Map();
+    for (const vote of currentVotes) {
+        if (!voteCounts.has(vote)) {
+            voteCounts.set(vote, 0);
+        }
+        voteCounts.set(vote, voteCounts.get(vote) + 1);
+    }
+    for (const [vote, count] of voteCounts.entries()) {
+        document.getElementById(`vote-result-${vote}`).innerHTML = count.toString();
+    }
+});
--- server/server.js
+++ server/server.js
@@ -1,61 +1,84 @@
-"use strict";
+'use strict';
 
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const publicPath = path.join(__dirname, "/../public");
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-
-
-let history = new Map(
-    // roomId => [
-    //     clientId => vote,
-    //     clientId => vote,
-    //     clientId => vote,
-    // ],
-    // roomId => [
-    //     clientid => vote,
-    //     clientId => vote,
-    //     clientId => vote,
-    // ]
-);
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
 
+let history = new Map();
+// roomId => [
+//     clientId => vote,
+//     clientId => vote,
+//     clientId => vote,
+// ],
+// roomId => [
+//     clientid => vote,
+//     clientId => vote,
+//     clientId => vote,
+// ]
+
 function getRoomVotes(roomId) {
+    ensureRoomHistory(roomId);
     return Array.from(history.get(roomId).values());
 }
 
 function setVote(roomId, clientId, value) {
+    ensureRoomHistory(roomId);
     const roomHistory = history.get(roomId);
     roomHistory.set(clientId, value);
 }
 
-function createRoomHistory(roomId) {
+function resetVotes(roomId) {
+    history.set(roomId, new Map());
+}
+
+function ensureRoomHistory(roomId) {
     if (!history.has(roomId)) {
-        history.set(roomId, new Map())
+        history.set(roomId, new Map());
     }
 }
 
+function broadcastVotes(roomId) {
+    io.in(roomId).emit('currentVotes', getRoomVotes(roomId));
+}
 
-io.on("connection", (socket) => {
-    console.log("A user just connected.");
+io.on('connection', socket => {
+    console.log('A user just connected.');
 
-    socket.on("start", ({ roomId }) => {
-        socket.join(roomId)
-        createRoomHistory(roomId);
-        //fetch history upon entering room
-        socket.emit("currentVotes", getRoomVotes(roomId));
-    })
+    socket.on('start', ({ roomId }) => {
+        socket.join(roomId);
+        socket.emit('currentVotes', getRoomVotes(roomId));
+    });
 
-    socket.on("vote", (vote) => {
-        setVote(vote.roomId, socket.client.id, vote.value);
-        console.log(history)
-        io.in(vote.roomId).emit("currentVotes", getRoomVotes(vote.roomId));
-    })
+    socket.on('vote', ({ roomId, value }) => {
+        setVote(roomId, socket.client.id, value);
+        io.in(roomId).emit('currentVotes', getRoomVotes(roomId));
+    });
+
+    socket.on('resetVotes', ({ roomId }) => {
+        resetVotes(roomId);
+        broadcastVotes(roomId);
+    });
+
+    socket.on('disconnect', reason => {
+        // Remove the client's vote on disconnection
+        history.forEach((room, roomId) => {
+            if (room.has(socket.client.id)) {
+                room.delete(socket.client.id);
+                if (room.size > 0) {
+                    broadcastVotes(roomId);
+                } else {
+                    history.delete(roomId);
+                }
+            }
+        });
+    });
 });
